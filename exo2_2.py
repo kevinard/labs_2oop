6e3b5044c98dc7cd450aac2e04269dfffe6c02ab
Kevin O'NEILL <contact@kevinoneill.fr>
2014-11-18 14:17:22 +0100

    Add exo 2.2

---
+++ exo2_2.py
@@ -0,0 +1,27 @@
+# 2.2
+#
+# Suite de Syracuse
+#
+# Ecrire une procédure affichant les n premiers termes de la suite de Syracuse de base a.
+
+
+def syracuse(n, a):
+    if type(a) is not int or a < 0:
+        print("Erreur : a doit être un entier positif.")
+
+    print(a, end=" ")
+
+    precedent = a
+
+    for i in range(1, n):
+        if precedent%2 == 0:
+            precedent //= 2
+        else:
+            precedent = 3*precedent+1
+
+        print(precedent, end=" ")
+
+    print("\n")
+
+
+syracuse(21, 15)
