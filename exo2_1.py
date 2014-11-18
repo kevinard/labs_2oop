a7579a5f4433357d02cf323186ff92a88372a2dc
Kevin O'NEILL <contact@kevinoneill.fr>
2014-11-18 14:36:54 +0100

    Update exo 2.1 - non recursive fibonacci function

--- exo2_1.py
+++ exo2_1.py
@@ -4,10 +4,31 @@
 #
 # Ecrire une fonction calculant le n ième terme de la suite de Fibonacci.
 
-# 0 1 1 2 3 5
+# 0 1 1 2 3 5 8 13 21 34 55
 
 
 def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    elif n >= 2:
+        precedent1 = 0
+        precedent2 = 1
+        terme = 0
+
+        for i in range(2, n+1):
+            terme = precedent1+precedent2
+            precedent1 = precedent2
+            precedent2 = terme
+
+        return terme
+    else:
+        print("Erreur : n doit être >= 0")
+        return -1
+
+
+def fibonacci_recursive(n):
     if n == 0:
         return 0
     elif n == 1:
