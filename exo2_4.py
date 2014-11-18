5e62a96cc2ef60fddb1897e499594b39e119eb16
Kevin O'NEILL <contact@kevinoneill.fr>
2014-11-18 14:25:01 +0100

    Add exo 2.4

---
+++ exo2_4.py
@@ -0,0 +1,15 @@
+# 2.4
+#
+# CALCUL DU MINIMUM D’UNE LISTE
+#
+# Ecrire une fonction calculant le plus petit élément d’une liste de données numériques.
+
+
+def getMin(l):
+    return min(l)
+
+maListe = [1, 2, 3, 4, 5]
+print(getMin(maListe))
+
+maListe = [234, 235, 678, 6874]
+print(getMin(maListe))
